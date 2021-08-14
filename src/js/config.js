fbecaa81030e412642834268d7a47fa0d96937cf
purvasikarwar12 <purvasikarwar@gmail.com>
2021-08-14 13:23:11 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2c60ade7-0bf4-4943-946c-466d898d154b';
+export const MODAL_CLOSE_SEC = 2.5;
